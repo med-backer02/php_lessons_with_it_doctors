ac093f169517f34d9811f332800f46fdd9b9f70c
Medet <frankpoco321@gmail.com>
2022-12-07 01:33:10 +0600

    add lesson-19 about comparison

---
+++ lesson-19/index.php
@@ -0,0 +1,15 @@
+<?php
+	$x = 1;
+	$y = '1';
+
+	echo $x < $y;
+	echo $x > $y;
+	echo $x <= $y;
+	echo $x >= $y;
+	echo $x == $y;
+	echo $x != $y;
+	echo $x <> $y;
+	echo $x === $y;
+	echo $x !== $y;
+	echo $x <=> $y;
+?>
