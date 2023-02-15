6a7dcf0a9b33106a7f994192869af731f3bae5b6
Medet <frankpoco321@gmail.com>
2023-02-15 23:42:40 +0600

    add lesson-33 about traversing an array with a for loop

---
+++ lesson-33/index.php
@@ -0,0 +1,25 @@
+<?php
+	$number = ['1','2','3'];
+	for($i=0;$i<count($number);$i++){
+		echo "{$number[$i]}";
+	}
+	$arr = [
+		'auto' => 'Dodge',
+		'plane' => 'Il-2'];
+	foreach ($arr as $key => $value) {
+		// code...
+		echo "$key : $value;<br>";
+	}
+
+	$transport = [
+		'auto' => ['lada','chevrolet'],
+		'planes' => ['il-2', 'i-16']
+		];
+	foreach($transport as $key => $array) {
+		echo "$key <br>";
+		foreach($array as $key => $value){
+			echo "$key <br>";
+			echo "$value <br>";
+		}
+	}
+?>
